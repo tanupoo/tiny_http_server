5b4b2b6abd136c2b06ef7e96a2551464fb0abdb3
Shoichi Sakane <github-shoichi@tanu.org>
2018-06-04 10:12:03 +0900

    support py2 and py3

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
-from chunkable_http_server import ChunkableHTTPRequestHandler
-from tiny_http_server import TinyHTTPHandler, ThreadedHTTPServer, TinyHTTPServer, DEBUG2, DEBUG3
+try:
+    from .chunkable_http_server import ChunkableHTTPRequestHandler
+    from .tiny_http_server import TinyHTTPHandler, ThreadedHTTPServer, TinyHTTPServer, DEBUG2, DEBUG3
+except:
+    from chunkable_http_server import ChunkableHTTPRequestHandler
+    from tiny_http_server import TinyHTTPHandler, ThreadedHTTPServer, TinyHTTPServer, DEBUG2, DEBUG3
